976292061b2dbc7b2a36bc826dce8a729747f11b
first_user <adityapatil24680@gmail.com>
2023-10-14 23:02:21 +0530

    common collections added

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -3,7 +3,13 @@ fn main() {
     for i in 1..10 {
         test_vector.push(i);
     }
-    for num in test_vector {
-        print!("{}", num);
+    let captain = test_vector.get(8);
+    match captain {
+        Some(&captain) => println!("Exists, {}", captain),
+        None => println!("Nope, doesn't exist!"),
+    }
+    
+    for i in &mut test_vector {
+        println!("{},", *i);
     }
 }
