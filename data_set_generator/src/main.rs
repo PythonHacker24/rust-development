a57fa9c2eb4485f9de0d8d0f3b31f7821ea3808f
first_user <adityapatil24680@gmail.com>
2023-10-17 17:20:00 +0530

    image dataset library added

--- data_set_generator/src/main.rs
+++ data_set_generator/src/main.rs
@@ -1,6 +1,5 @@
 extern crate image; 
 
-use std::env;
 use image::GenericImageView;
 
 fn image_to_vector(location: String) -> Vec<Vec<Vec<u8>>>{
@@ -38,36 +37,12 @@ fn rgb_to_intensity_vector(image_vector: Vec<Vec<Vec<u8>>>) -> Vec<Vec<u8>> {
     return output;
 }
 
-fn characters_vector(intensity_vector: Vec<Vec<u8>>) -> Vec<Vec<char>> {
-    let mut output: Vec<Vec<char>> = Vec::new();
-    for horizontal_vector in intensity_vector {
-        let mut char_horizontal_vector: Vec<char> = Vec::new();
-        for pixel_value in horizontal_vector {
-            if pixel_value < 50 { char_horizontal_vector.push('■') }
-            else if pixel_value >=80 && pixel_value < 100 { char_horizontal_vector.push(' ') }
-            else if pixel_value >=100 && pixel_value < 120 { char_horizontal_vector.push('&') }
-            else if pixel_value >= 120 && pixel_value < 140 { char_horizontal_vector.push('=') }
-            else if pixel_value >=140 && pixel_value < 160 { char_horizontal_vector.push('+') }
-            else if pixel_value >= 160 && pixel_value < 180 { char_horizontal_vector.push(';')}
-            else if pixel_value >= 180 && pixel_value < 220 { char_horizontal_vector.push(':') }
-            else if pixel_value >= 220 && pixel_value < 240 { char_horizontal_vector.push(',') }
-            else if pixel_value >=240 && pixel_value < 255 { char_horizontal_vector.push('.') }
-            else { char_horizontal_vector.push(' ') } 
-        }
-        output.push(char_horizontal_vector);
-    }
-    return output;
-}
+
 
 fn main() {
     
-    let args: Vec<String> = env::args().skip(1).collect();
-    if args.is_empty() {
-        println!("[*] Path to image not provided!");
-        std::process::exit(1);
-    }
-
-    let image_vector: Vec<Vec<Vec<u8>>> = image_to_vector(args[0].clone()); 
+    let image_link: String = "Image link";
+    let image_vector: Vec<Vec<Vec<u8>>> = image_to_vector(image_link.to_string()); 
     let intensity_vector: Vec<Vec<u8>> = rgb_to_intensity_vector(image_vector);
     let character_image_vector: Vec<Vec<char>> = characters_vector(intensity_vector);
 
